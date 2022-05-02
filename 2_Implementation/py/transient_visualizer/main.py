Fixed normalization issue and cleaned the code
Former-commit-id: 0b79471697066e6f991236e37feee5c912bd434a
Former-commit-id: d52b7e686104480341633b1b766271030682efcc
Former-commit-id: 65a2e90092a6f593aa465576ba187720ff17a90f [formerly 832c625835264b3254503cd2122c624b1ef6bba7]
Former-commit-id: dda2c68c6ec87a771aaa06d73b9de0dd29d4d26f

2_Implementation/py/transient_visualizer/main.py
@@ -123,10 +123,10 @@ def reshape_frame(files):
         np_pt = np.float32
 
     # Define an empty matrix of size image_height x image_width x temporal_samples for each channel
-    frame_A = np.empty([size[1], size[1], size[0]])
-    frame_R = np.empty([size[1], size[1], size[0]])
-    frame_G = np.empty([size[1], size[1], size[0]])
-    frame_B = np.empty([size[1], size[1], size[0]])
+    frame_A = np.empty([len(files), size[1], size[0]])
+    frame_R = np.empty([len(files), size[1], size[0]])
+    frame_G = np.empty([len(files), size[1], size[0]])
+    frame_B = np.empty([len(files), size[1], size[0]])
 
     # Set initial value to the global min e global max (updated later)
     global_max = 0