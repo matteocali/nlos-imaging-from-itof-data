4a70f7188f2002965febf5726671fa23ce8dcd11
DECaligM <matteo.caligiuri@sony.com>
2022-05-10 14:09:34 +0200

    Add the total_image_tester.py script to compare the original render with the image obtained by summing up all the transient images
    Former-commit-id: 752b069ccbbc4e844aa23c55814e62fc45b727ed
    Former-commit-id: 609b70814cc442c8e8010268c013e859c3743e70
    Former-commit-id: d7ffaf9c6792558dede047566e8ce507f6348b21 [formerly 66db4585048556e2e4bd74bb847eeab990c6a54a]
    Former-commit-id: 8a49a284ff75a645e120c9b0558099e9708ce1ec

--- 2_Implementation/py/total_image_tester/total_image_tester.py
+++ 2_Implementation/py/total_image_tester/total_image_tester.py
@@ -6,14 +6,12 @@ import glob
 import time
 import warnings
 from natsort import natsorted
-
-import numpy
 from tqdm import tqdm
-
 import OpenEXR
 import imageio
 import Imath
 import numpy as np
+from matplotlib import pyplot as plt, cm, colors
 
 
 def arg_parser(argv):
@@ -24,11 +22,12 @@ def arg_parser(argv):
     """
 
     arg_input = os.getcwd()  # Argument containing the input directory
+    arg_img = ""  # Argument containing the path of the target image
     arg_output = "total_image"  # Argument containing the output directory
-    arg_help = "{0} -i <input> -o <output>".format(argv[0])  # Help string
+    arg_help = "{0} -i <input> -m <image> -o <output>".format(argv[0])  # Help string
 
     try:
-        opts, args = getopt.getopt(argv[1:], "hi:o:", ["help", "input=", "output="])  # Recover the passed options and arguments from the command line (if any)
+        opts, args = getopt.getopt(argv[1:], "hi:m:o:", ["help", "input=", "image=", "output="])  # Recover the passed options and arguments from the command line (if any)
     except:
         print(arg_help)  # If the user provide a wrong options print the help string
         sys.exit(2)
@@ -39,14 +38,17 @@ def arg_parser(argv):
             sys.exit(2)
         elif opt in ("-i", "--input"):
             arg_input = Path(arg)  # Set the input directory
+        elif opt in ("-m", "--image"):
+            arg_img = Path(arg)  # Set the path of the target image
         elif opt in ("-o", "--output"):
             arg_output = arg  # Set the output directory
 
     print('Input folder:', arg_input)
+    print('Input image:', arg_img)
     print('Output file name:', arg_output)
     print()
 
-    return [arg_input, arg_output]
+    return [arg_input, arg_img, arg_output]
 
 
 def create_folder(path):
@@ -79,6 +81,42 @@ def reed_files(path, extension, reorder=True):
     return files
 
 
+def load_exr(path):
+    """
+    Function that load an exr image
+    :param path: path of the image
+    :return: a numpy matrix containing al the channels ([A, R, G, B] or [R, G, B])
+    """
+    img = OpenEXR.InputFile(path)  # Open the file
+    n_channels = len(img.header()['channels'])  # Extract the number of channels of the image
+    dw = img.header()['dataWindow']  # Extract the data window dimension from the header of the exr file
+    size = (dw.max.x - dw.min.x + 1, dw.max.y - dw.min.y + 1)  # Define the actual size of the image
+    pt = OpenEXR.InputFile(files[0]).header()['channels']['R'].type  # Recover the pixel type from the header
+
+    # Check if the pt is HALF (pt.v == 1) or FLOAT (pt.v == 2)
+    if pt.v == Imath.PixelType.HALF:
+        np_pt = np.float16  # Define the correspondent value in numpy (float16 or float32)
+    elif pt.v == Imath.PixelType.FLOAT:
+        np_pt = np.float32
+
+    if n_channels == 3:
+        (R, G, B) = [np.frombuffer(img.channel(Chan, pt), dtype=np_pt) for Chan in ("R", "G", "B")]  # Extract the four channel from each image
+        if np_pt == np.float16:
+            (R, G, B) = [data.reshape(size[1], -1).astype(np.float32) for data in [R, G, B]]  # Reshape each vector to match the image size
+        else:
+            (R, G, B) = [data.reshape(size[1], -1) for data in [R, G, B]]  # Reshape each vector to match the image size
+
+        return np.stack((R, G, B), axis=2)
+    elif n_channels == 4:
+        (A, R, G, B) = [np.frombuffer(img.channel(Chan, pt), dtype=np_pt) for Chan in ("A", "R", "G", "B")]  # Extract the four channel from each image
+        if np_pt == np.float16:
+            (A, R, G, B) = [data.reshape(size[1], -1).astype(np.float32) for data in [A, R, G, B]]  # Reshape each vector to match the image size
+        else:
+            (A, R, G, B) = [data.reshape(size[1], -1) for data in [A, R, G, B]]  # Reshape each vector to match the image size
+
+        return np.stack((A, R, G, B), axis=2)
+
+
 def reshape_frame(files):
     """
     Function that load al the exr file in the input folder and reshape it in order to have three matrices, one for each channel containing all the temporal value
@@ -91,13 +129,6 @@ def reshape_frame(files):
 
     dw = OpenEXR.InputFile(files[0]).header()['dataWindow']    # Extract the data window dimension from the header of the exr file
     size = (dw.max.x - dw.min.x + 1, dw.max.y - dw.min.y + 1)  # Define the actual size of the image
-    pt = OpenEXR.InputFile(files[0]).header()['channels']['R'].type  # Recover the pixel type from the header
-
-    # Check if the pt is HALF (pt.v == 1) or FLOAT (pt.v == 2)
-    if pt.v == Imath.PixelType.HALF:
-        np_pt = np.float16  # Define the correspondent value in numpy (float16 or float32)
-    elif pt.v == Imath.PixelType.FLOAT:
-        np_pt = np.float32
 
     # Define an empty matrix of size image_height x image_width x temporal_samples for each channel
     frame_A = np.empty([len(files), size[1], size[0]], dtype=np.float32)
@@ -106,20 +137,14 @@ def reshape_frame(files):
     frame_B = np.empty([len(files), size[1], size[0]], dtype=np.float32)
 
     for index, file in enumerate(tqdm(files)):  # For each provided file in the input folder
-        img = OpenEXR.InputFile(file)  # Open each file
-
-        (A, R, G, B) = [np.frombuffer(img.channel(Chan, pt), dtype=np_pt) for Chan in ("A", "R", "G", "B")]  # Extract the four channel from each image
-        if np_pt == np.float16:
-            (A, R, G, B) = [data.reshape(size[1], -1).astype(np.float32) for data in [A, R, G, B]]  # Reshape each vector to match the image size
-        else:
-            (A, R, G, B) = [data.reshape(size[1], -1) for data in [A, R, G, B]]  # Reshape each vector to match the image size
+        img = load_exr(file)
 
         # Perform the reshaping saving the results in frame_i for i in A, R,G , B
         for i in range(size[0]):
-            frame_A[index, :, i] = A[:, i]
-            frame_R[index, :, i] = R[:, i]
-            frame_G[index, :, i] = G[:, i]
-            frame_B[index, :, i] = B[:, i]
+            frame_A[index, :, i] = img[:, i, 0]
+            frame_R[index, :, i] = img[:, i, 1]
+            frame_G[index, :, i] = img[:, i, 2]
+            frame_B[index, :, i] = img[:, i, 3]
 
         warnings.filterwarnings('ignore')  # Remove warning about the presence of matrix completely empty (full of nan)
 
@@ -142,19 +167,17 @@ def img_matrix(channels):
 
     print(f"Build the {np.shape(frame_A)[2]} image matrices:")
     time.sleep(0.02)
-    images = []  # Empty list that will contain all the images
+    images = np.empty([np.shape(frame_A)[2], np.shape(frame_A)[0], np.shape(frame_A)[1], len(channels)], dtype=np.float32)  # Empty array that will contain all the images
     # Fuse the channels together to obtain a proper [A, R, G, B] image
     for i in tqdm(range(np.shape(frame_A)[2])):
-        img = numpy.empty([np.shape(frame_A)[0], np.shape(frame_A)[1], len(channels)], dtype=np.float32)  # Create an empty numpy array of the correct shape
+        images[i, :, :, 0] = frame_R[:, :, i]
+        images[i, :, :, 1] = frame_G[:, :, i]
+        images[i, :, :, 2] = frame_B[:, :, i]
+        images[i, :, :, 3] = frame_A[:, :, i]
 
-        img[:, :, 0] = frame_R[:, :, i]
-        img[:, :, 1] = frame_G[:, :, i]
-        img[:, :, 2] = frame_B[:, :, i]
-        img[:, :, 3] = frame_A[:, :, i]
+        images[i, :, :, :][np.isnan(frame_A[:, :, i])] = 0  # Remove all the nan value following the Alpha matrix
 
-        img[np.isnan(frame_A[:, :, i])] = 0  # Remove all the nan value following the Alpha matrix
-
-        images.append(img)
+    np.save("np_images.npy", np.asarray(images))  # Save the loaded images as a numpy array
 
     end = time.time()
     print("Images created successfully in %.2f sec\n" % (round((end - start), 2)))
@@ -165,13 +188,14 @@ def img_matrix(channels):
 def total_img(images, output):
     """
     Function to build the image obtained by sum all the temporal instant of the transient
-    :param images: list of all the images
+    :param images: np.array containing of all the images
     :param output: output path
+    :return: total image as a numpy matrix
     """
     print("Generate the total image = sum over all the time instants")
     start = time.time()  # Compute the execution time
 
-    summed_images = np.nansum(np.asarray(images)[:, :, :, :-1], axis=0)  # Sum all the produced images over the time dimension ignoring the alpha channel
+    summed_images = np.nansum(images[:, :, :, :-1], axis=0)  # Sum all the produced images over the time dimension ignoring the alpha channel
 
     # Generate a mask matrix that will contain the number of active beans in each pixel (needed to normalize the image)
     mask = np.zeros([images[0].shape[0], images[0].shape[1]])
@@ -188,14 +212,96 @@ def total_img(images, output):
     end = time.time()
     print("Process concluded in %.2f sec\n" % (round((end - start), 2)))
 
+    return total_image
+
+
+def compute_mse(x, y):
+    """
+    Compute the MSE error between two images
+    The 'Mean Squared Error' between the two images is the sum of the squared difference between the two images
+    The lower the error, the more "similar" the two images are
+    code from: https://pyimagesearch.com/2014/09/15/python-compare-two-images/
+    :param x: image 1
+    :param y: image 2 (must have same dimension of image 1)
+    :return The MSE value rounded at the fourth value after comma
+    """
+
+    err = np.sum((x.astype("float") - y.astype("float")) ** 2)  # Convert the images to floating point
+                                                                # Take the difference between the images by subtracting the pixel intensities
+                                                                # Square these difference and sum them up
+    err /= float(x.shape[0] * x.shape[1])  # Handles the mean of the MSE
+
+    return round(err, 4)
+
+
+def img_comparison(o_img, t_img):
+    """
+    Function to plot the comparison between the real image and the one obtained by summing the transient over the temporal direction (+ compute the MSE)
+    :param o_img: original image [R, G, B]
+    :param t_img: transient image [R, G, B]
+    """
+    print("Compare the original images with the one obtained summing all the transient ones")
+    print(f"The MSE is {compute_mse(o_img, t_img)}")
+
+    # Extract the minimum and maximum displayed value to normalize the colors
+    min_val = min([np.min(o_img), np.min(t_img)])
+    max_val = max([np.max(o_img), np.max(t_img)])
+
+    # Plot each channel of both the image, together with the colorbar
+    fig, axs = plt.subplots(3, 2, figsize=(15, 15))
+    axs[0, 0].matshow(o_img[:, :, 0], cmap=cm.get_cmap("jet"), norm=colors.Normalize(vmin=min_val, vmax=max_val))
+    axs[0, 0].set_title("Red channel of the original image")
+    axs[1, 0].matshow(o_img[:, :, 1], cmap=cm.get_cmap("jet"), norm=colors.Normalize(vmin=min_val, vmax=max_val))
+    axs[1, 0].set_title("Green channel of the original image")
+    axs[2, 0].matshow(o_img[:, :, 2], cmap=cm.get_cmap("jet"), norm=colors.Normalize(vmin=min_val, vmax=max_val))
+    axs[2, 0].set_title("Blu channel of the original image")
+    axs[0, 1].matshow(t_img[:, :, 0], cmap=cm.get_cmap("jet"), norm=colors.Normalize(vmin=min_val, vmax=max_val))
+    axs[0, 1].set_title("Red channel of the transient image")
+    axs[1, 1].matshow(t_img[:, :, 1], cmap=cm.get_cmap("jet"), norm=colors.Normalize(vmin=min_val, vmax=max_val))
+    axs[1, 1].set_title("Green channel of the transient image")
+    axs[2, 1].matshow(t_img[:, :, 2], cmap=cm.get_cmap("jet"), norm=colors.Normalize(vmin=min_val, vmax=max_val))
+    axs[2, 1].set_title("Blu channel of the transient image")
+    fig.colorbar(cm.ScalarMappable(norm=colors.Normalize(vmin=min_val, vmax=max_val), cmap=cm.get_cmap('jet')), ax=axs, label=r"Radiance [$W/(m^{2}·sr)$]")
+    plt.savefig("channel_comparison.svg")
+    fig.show()
+
+    # Compute the differences between the original and transient image, channel by channel
+    R_diff = t_img[:, :, 0] - o_img[:, :, 0]
+    G_diff = t_img[:, :, 1] - o_img[:, :, 1]
+    B_diff = t_img[:, :, 2] - o_img[:, :, 2]
+
+    # Extract the minimum and maximum displayed value to normalize the colors
+    min_val = min([np.min(R_diff), np.min(G_diff), np.min(B_diff)])
+    max_val = max([np.max(R_diff), np.max(G_diff), np.max(B_diff)])
+
+    # Plot the difference between the two images, channel by channel
+    fig2, axs2 = plt.subplots(1, 3, figsize=(18, 6))
+    axs2[0].matshow(R_diff, cmap=cm.get_cmap("jet"), norm=colors.Normalize(vmin=min_val, vmax=max_val))
+    axs2[0].set_title("Difference on the red channel")
+    axs2[1].matshow(G_diff, cmap=cm.get_cmap("jet"), norm=colors.Normalize(vmin=min_val, vmax=max_val))
+    axs2[1].set_title("Difference on the green channel")
+    axs2[2].matshow(B_diff, cmap=cm.get_cmap("jet"), norm=colors.Normalize(vmin=min_val, vmax=max_val))
+    axs2[2].set_title("Difference on the blu channel")
+    fig2.colorbar(cm.ScalarMappable(norm=colors.Normalize(vmin=min_val, vmax=max_val), cmap=cm.get_cmap('jet')), ax=axs2, orientation="horizontal")
+    plt.savefig("channel_differences.svg")
+    fig2.show()
+
+    print("Press enter to end ...")
+    input()  # Wait for a keystroke to close the windows
+
 
 if __name__ == '__main__':
-    arg_input, arg_output = arg_parser(sys.argv)  # Recover the input and output folder from the console args
+    arg_input, arg_img, arg_output = arg_parser(sys.argv)  # Recover the input and output folder from the console args
 
     files = reed_files(str(arg_input), "exr")  # Load the path of all the files in the input folder with extension .exr
 
-    (frame_A, frame_R, frame_G, frame_B) = reshape_frame(files)  # Reshape the frame in a standard layout
+    if not os.path.exists(Path("np_images.npy")):
+        (frame_A, frame_R, frame_G, frame_B) = reshape_frame(files)  # Reshape the frame in a standard layout
 
-    images = img_matrix([frame_A, frame_R, frame_G, frame_B])  # Create the image files
+        images = img_matrix([frame_A, frame_R, frame_G, frame_B])  # Create the image files
+    else:  # If already exists a npy file containing all the transient images load it instead of processing everything again
+        images = np.load("np_images.npy")
 
-    total_img(images, arg_output)  # Create and save the total image
+    total_image = total_img(images, arg_output)  # Create and save the total image
+
+    img_comparison(load_exr(str(arg_img))[:, :, 1 :], total_image)  # Compare the original render with the one obtained by summing up all the transient images
