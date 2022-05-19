Fixed normalization
Former-commit-id: 9b159028637992e0c2232630501cc07dc454f3f3
Former-commit-id: 94f7f2347d361bb1eba356c57bf6d57bb8ba1c67
Former-commit-id: 9e1b1b49fd1f3812e9cdb67639fcbf76e1cbe6e9 [formerly 90e1661e687c88434eb96765d0a89024eec7e60e]
Former-commit-id: 07aa621252eb10394ac33b8730ffd635706d084d

2_Implementation/py/test/tester.py
@@ -5,6 +5,7 @@ import os
 from os.path import exists
 import getopt
 import sys
+import time
 
 
 def arg_parser(argv):
@@ -38,7 +39,6 @@ def arg_parser(argv):
 
     print('Input path:', arg_in)
     print('Output path:', arg_out)
-    print('task:', arg_task)
     print()
 
     return [arg_in, arg_out, arg_task]
@@ -48,16 +48,28 @@ if __name__ == '__main__':
     arg_in, arg_out, arg_task = arg_parser(sys.argv)  # Recover the input and output folder from the console args
 
     if arg_task == "tr_video":
+        print(f"TASK: {arg_task}")
+        start = time.time()
+
         images = tr.transient_loader(img_path=arg_in, np_path=arg_out / "np_transient.npy", store=(not exists(arg_out / "np_transient.npy")))  # Load the transient
         ut.create_folder(arg_out)  # Create the output folder if not already present
         tr.transient_video(images, arg_out)  # Generate the video
+
+        end = time.time()
+        print(f"Task <{arg_task}> concluded in in %.2f sec\n" % (round((end - start), 2)))
     elif arg_task == "total_img":
         images = tr.transient_loader(img_path=arg_in, np_path=arg_out / "np_transient.npy", store=(not exists(arg_out / "np_transient.npy")))  # Load the transient
         tr.total_img(images, arg_out / "total_image", normalize=False)
     elif arg_task == "glb_tr_video":
+        print(f"TASK: {arg_task}")
+        start = time.time()
+
         images = tr.transient_loader(img_path=arg_in, np_path=arg_out / "np_transient.npy", store=(not exists(arg_out / "np_transient.npy")))  # Load the transient
         glb_images = tr.rmv_first_reflection(images=images, file_path=arg_out / "glb_np_transient.npy", store=(not exists(arg_out / "glb_np_transient.npy")))
         tr.transient_video(np.copy(glb_images), arg_out, normalize=True)
         tr.total_img(glb_images, arg_out / "total_image", normalize=False)
+
+        end = time.time()
+        print(f"Task <{arg_task}> concluded in in %.2f sec\n" % (round((end - start), 2)))
     elif arg_task == "spot_bitmap":
         ut.spot_bitmap_gen(arg_out / "spot_bitmap.png", [640, 480], 10)