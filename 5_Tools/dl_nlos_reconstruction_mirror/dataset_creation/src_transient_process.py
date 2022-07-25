be8009e8c3c6a4a75f55560c48eaf0196c2f0eb0
DECaligM <matteo.caligiuri@sony.com>
2022-07-25 16:16:19 +0200

    Adapted the dataset creation function to mine data
    Former-commit-id: 42c56d5bdedd62fa084e8e97bd96752086175e14
    Former-commit-id: d6235ef637d89fe3bfaa16e8fe84349093c1c95c [formerly 7163e35648ab9356f8a4278b09876ae7b06da9da] [formerly 5f173d70bf7255fb327aaf17713700e18179b081 [formerly a3b9b36cfb567001b1516ac292410a439a6b31a6]] [formerly cce76fc3e1cc20879ee86fc6ee727576ce7ad92b [formerly 81ac0805d6ff4e864ad0329d282ab7de23a80c24] [formerly a29e73bb35bceb8118db0145541f694e8ccd3721]]
    Former-commit-id: 6317baee720d8c8297426b81814bf4631111fb2f [formerly 31161a6594c7846e4d77abef9997c7475be0a124] [formerly 926ead7379518d0e126a50cd6da2abbc5568a333]
    Former-commit-id: 52276a692bce3a7660d4d1d0392d64890353a512 [formerly 56056356e597ce242e1c55ae159f453f096cecd2]
    Former-commit-id: 8aa311f4639a2e0a7f8a203ad7c9ad9b254522dd
    Former-commit-id: e63fb616ebf354962274509ea57eed2f51100577 [formerly 00fa9d9e16e4241a82bc81a94df94b5bde1ecdea]
    Former-commit-id: ef2493a17b5b2620f4d0b3559c364c06d56feb45

--- 5_Tools/dl_nlos_reconstruction_mirror/dataset_creation/src_transient_process.py
+++ 5_Tools/dl_nlos_reconstruction_mirror/dataset_creation/src_transient_process.py
@@ -1,17 +1,22 @@
-import random
-import os, sys
-sys.path.append("./src/")
-sys.path.append("../utils/")
+import os
+import sys
 import h5py
 import numpy as np
 import glob
 import csv
+import time
+sys.path.append("./src")
+sys.path.append("../utils")
+from fun_acquire_pixels import acquire_pixels
+from fct_Aphi_compute import Aphi_compute
+
+'''
 flag_aug = False
 if flag_aug:
     from fun_acquire_pixels_kxk import acquire_pixels
 else:
     from fun_acquire_pixels import acquire_pixels
-from fct_Aphi_compute import Aphi_compute
+'''
 
 """
 
@@ -21,123 +26,114 @@ For more details of the data used see fun_acquire_pixels where most of the compu
 The data is saved in the "data" folder under the training directory
 
 Flags and variables:
-
     -out_dir:                   path to the output directory (by default the "data" folder in the training directory)
     -data_dir:                  location of the transient dataset
     -flag_new_shuffle:          Set to True to get a new random split of the dataset images. Otherwise the csv files in ./data_split/ will be used
     -flag_ow:                   Set to True also uses single walls images. Otherwise only images with MPI will be used 
     -flag_fit:                  Set to True performs the fitting of the Weibull functions on the training data 
-    -f_ran:                     Set to True chooses the patches in random positions from all over the image, otherwise a grid willl be employed
+    -f_ran:                     Set to True chooses the patches in random positions from all over the image, otherwise a grid will be employed
     -n_patches:                 Number of patches taken from each images
     -s:                         Patch size
     -max_imgs:                  Maximum number of images of each set. Can be used to make a quick run of the process. Otherwise set it to high value (higher than max dataset size)
     -train_slice:               Percentage of images belonging to training dataset expressed as a number between 0 and 1. Test and validation will take each half of what remains
-    -fl_get_train:              If set to True we will acquire the training set. Same for the validation and test ones.
-    -fl_get_val:
-    -fl_get_test:
+    -fl_get_train:              If set to True we will acquire the training set
+    -fl_get_val:                If set to True we will acquire the validation set
+    -fl_get_test:               If set to True we will acquire the test set
     -fl_normalize_transient:    Whether to normalize all transient to sum to 1. Can be useful but it is better to do it later right before training
-    
 """
 
 
-out_dir = "../training/data/"    # Output directory
+start_time = time.time()
+
+out_dir = "../training/data/"    # output directory
 data_dir = "../dataset_rec/"  # dataset directory
+rnd_seed = 2019283  # random seed
+np.random.seed(rnd_seed)
 
 if not os.path.exists(out_dir):
     os.mkdir(out_dir)
 
-flag_new_shuffle = True#False  # whether to shuffle again the images and create new training validation and test datasets
+flag_new_shuffle = True  # whether to shuffle again the images and create new training validation and test datasets
 flag_ow = True  # whether to use also one wall images or not
 flag_fit = False  # whether to fit the data using weibull functions (slows down the code)
-f_ran = True
+f_ran = True  # whether to use random or grid sampling
+
 n_patches = 200  # number of pixels taken from each image
 s = 3  # size of each input patch
-add_str = "_s" + str(s)  # Dataset name
-if flag_aug:
-    add_str += "_aug"
-max_imgs=1000
-train_slice = 0.6
-val_slice = (1-train_slice)/2
-freqs = np.array((20e06, 50e06, 60e06),dtype=np.float32)
-#freqs = np.array((20e06,100e06),dtype=np.float32)
+add_str = f"_s{s}"  # part of the dataset name containing the patch size
+
+max_imgs = 1000  # maximum number of images to be used (if grater than the actual number, all the dataset will be used)
+train_slice = 0.6  # percentage of images belonging to training dataset
+val_slice = round((1 - train_slice) / 2, 1)  # percentage of images belonging to validation dataset
+freqs = np.array((20e06, 50e06, 60e06), dtype=np.float32)  # frequencies used by the iToF sensor
 if freqs.shape[0] == 2:
     add_str += "_2freq"
 
 # Choose which datasets you want to build
 fl_get_train = True
-fl_get_val = False#True
-fl_get_test = False#True
-fl_normalize_transient = False  # Whether to normalize the transient information 
+fl_get_val = True
+fl_get_test = True
+fl_normalize_transient = False  # whether to normalize the transient information
 
 # Grab all the names of the images of the dataset, shuffle them and save them in a csv file
 if not fl_normalize_transient:
     add_str += "_nonorm"
 
 if flag_new_shuffle:
-    path = data_dir + "*.h5"    # Path to the transient dataset
-    filenames = glob.glob(path)
-    num_img = len(filenames)
-    indexes = np.arange(num_img)
-    indexes = np.random.permutation(indexes)
-    indexes = np.array(indexes)
-    filenames = np.array(filenames)
-    filenames = filenames[indexes]
-    for i,file in enumerate(filenames):
+    # Load all the images of the dataset
+    path = data_dir + "*.h5"  # Path to the transient dataset
+    filenames = glob.glob(path)  # Get all the names of the files
+
+    # Shuffle the images
+    num_img = len(filenames)  # Number of images in the dataset
+    indexes = np.arange(num_img)  # Create an array with the indexes of the images
+    indexes = np.random.permutation(indexes)  # Shuffle the indexes
+    indexes = np.array(indexes)  # Convert the indexes to an array
+    filenames = np.array(filenames)  # Convert the names to an array
+    filenames = filenames[indexes]  # Shuffle the names according to the indexes
+
+    for i, file in enumerate(filenames):
         filenames[i] = file.replace("\\", "/")
-    with open("shuffled_images.csv", "w", newline="") as csvfile:
+
+    with open("shuffled_images.csv", "w", newline="") as csvfile:  # Save the names of the images in a csv file
         wr = csv.writer(csvfile)
         for filename in filenames:
             wr.writerow([filename])
 
-    file_list = []
-    with open("shuffled_images.csv", "r") as csvfile:
+    images_list = []
+    with open("shuffled_images.csv", "r") as csvfile:  # Load the names of the images from the csv file
         wr = csv.reader(csvfile)
         for row in wr:
-            file_list.append(row)
+            images_list.append(row)
 
-    file_list = [item for sublist in file_list for item in sublist]
-    owalls = [s for s in file_list if "cube" in s]
-    twalls_ang = [s for s in file_list if "sphere" in s]
-    twalls = [s for s in file_list if (("twalls" in s) and ("angle" not in s))]
-    three_walls = [s for s in file_list if "3walls" in s]
+    images_list = [item for sublist in images_list for item in sublist]  # Flatten the list of lists
 
-    images_list = [file_list]#[owalls, twalls_ang, twalls, three_walls]
+    # Split the images in training, validation and test sets
+    num_img = len(images_list)  # Number of images in the dataset
+    n_train = int(np.round(train_slice * num_img))  # Number of images in the training set
+    n_val = int(np.round(val_slice * num_img))  # Number of images in the validation set
+    n_test = num_img - n_train - n_val  # Number of images in the test set
+    train_files = images_list[:n_train]  # Names of the images in the training set
+    val_files = images_list[n_train:n_train+n_val]  # Names of the images in the validation set
+    test_files = images_list[n_train+n_val:]  # Names of the images in the test set
+    np.random.shuffle(test_files)  # Shuffle the test set
+    np.random.shuffle(val_files)  # Shuffle the validation set
+    np.random.shuffle(train_files)  # Shuffle the training set
 
-
-    train_files = []
-    val_files = []
-    test_files = []
-    for images in images_list:
-        num_img = len(images)
-        n_train = int(np.round(train_slice*num_img))
-        n_val = int(np.round(val_slice*num_img))
-        n_test = num_img-n_train-n_val
-        train_tmp = images[:n_train]
-        val_tmp = images[n_train:n_train+n_val]
-        test_tmp = images[n_train+n_val:]
-        train_files += train_tmp
-        val_files += val_tmp
-        test_files += test_tmp
-    np.random.shuffle(test_files)
-    np.random.shuffle(val_files)
-    np.random.shuffle(train_files)
-
-
-
-    with open("./data_split/train_images.csv","w",newline="") as csvfile:
+    with open("./data_split/train_images.csv", "w", newline="") as csvfile:  # Save the names of the images in the train_dataset in a csv file
         wr = csv.writer(csvfile)
         for filename in train_files:
             wr.writerow([os.path.basename(filename)])
-    with open("./data_split/val_images.csv","w",newline="") as csvfile:
+    with open("./data_split/val_images.csv", "w", newline="") as csvfile:  # Save the names of the images in the val_dataset in a csv file
         wr = csv.writer(csvfile)
         for filename in val_files:
             wr.writerow([os.path.basename(filename)])
-    with open("./data_split/test_images.csv","w",newline="") as csvfile:
+    with open("./data_split/test_images.csv", "w", newline="") as csvfile:  # Save the names of the images in the test_dataset in a csv file
         wr = csv.writer(csvfile)
         for filename in test_files:
             wr.writerow([os.path.basename(filename)])
 
-
+# Load the names of the images from the csv file, considering the split in training, validation and test sets
 train_files = []
 val_files = []
 test_files = []
@@ -155,102 +151,104 @@ with open("./data_split/test_images.csv", "r") as csvfile:
     for row in wr:
         test_files.append(os.path.basename(row[0]))
 
-N_train = len(train_files)
-N_val = len(val_files)
-N_test = len(test_files)
+N_train = len(train_files)  # Number of images in the training set
+N_val = len(val_files)  # Number of images in the validation set
+N_test = len(test_files)  # Number of images in the test set
 
-print(N_train,N_val,N_test)
+print(f"Training set size: {N_train}")
+print(f"Validation set size: {N_val}")
+print(f"Test set size: {N_test}\n")
 
 # TRAINING IMAGES
-datasetname = 1    # Flag used to keep track of the used dataset
-
-
+datasetname = "test"    # Flag used to keep track of the used dataset
 
 if fl_get_train:
     train_files = np.asarray(train_files)
-    train_files = [data_dir + fil for fil in train_files]
-    print("Training dataset")
-
-
-    Back, Back_nod, Fit_Parameters, peak_ind, peak_val, v_real, v_real_no_d, v_real_d, phi, Back_fit = acquire_pixels(train_files,n_patches,max_imgs,f_ran,s,flag_ow,flag_fit,fl_normalize_transient,freqs)
+    train_files = [data_dir + file for file in train_files]
+    print("Training dataset:")
+    Back, Back_nod, gt_real, Fit_Parameters, peak_ind, peak_val, v_real, v_real_no_d, v_real_d, phi, Back_fit = acquire_pixels(train_files, n_patches, max_imgs, f_ran, s, flag_ow, flag_fit, fl_normalize_transient, freqs)
     num_elem = Back.shape[0]
 
-    A_in,phi_in,A_g,phi_g,A_d,phi_d = Aphi_compute(v_real,v_real_no_d,v_real_d)
+    A_in, phi_in, A_g, phi_g, A_d, phi_d = Aphi_compute(v_real, v_real_no_d, v_real_d)
 
-    file_train = out_dir + "train_walls_"+str(num_elem)+add_str+".h5"
-    with h5py.File(file_train,'w') as f:
+    file_train = out_dir + "train_n" + str(num_elem) + add_str + ".h5"
+    with h5py.File(file_train, 'w') as f:
         dset = f.create_dataset("name", data=datasetname)
-        dset = f.create_dataset("transient", data=Back, compression="gzip") # Conviene togliere la compressione
+        dset = f.create_dataset("transient", data=Back, compression="gzip")
         dset = f.create_dataset("transient_global", data=Back_nod, compression="gzip")
+        dset = f.create_dataset("gt", data=gt_real, compression="gzip")
         dset = f.create_dataset("raw_itof", data=v_real)
         dset = f.create_dataset("global_itof", data=v_real_no_d)
         dset = f.create_dataset("direct_itof", data=v_real_d)
-        dset = f.create_dataset("amplitude_raw",data = A_in)
-        dset = f.create_dataset("phase_raw",data = phi_in)
-        dset = f.create_dataset("amplitude_direct",data = A_d)
-        dset = f.create_dataset("phase_direct",data = phi_d)
-        dset = f.create_dataset("amplitude_global",data = A_g)
-        dset = f.create_dataset("phase_global",data = phi_g)
-        dset = f.create_dataset("freqs",data = freqs)
+        dset = f.create_dataset("amplitude_raw", data=A_in)
+        dset = f.create_dataset("phase_raw", data=phi_in)
+        dset = f.create_dataset("amplitude_direct", data=A_d)
+        dset = f.create_dataset("phase_direct", data=phi_d)
+        dset = f.create_dataset("amplitude_global", data=A_g)
+        dset = f.create_dataset("phase_global", data=phi_g)
+        dset = f.create_dataset("freqs", data=freqs)
         if flag_fit:
-            dset = f.create_dataset("transient_fit",data = Back_fit)
-
-
+            dset = f.create_dataset("transient_fit", data=Back_fit)
 
 # VALIDATION IMAGES
 if fl_get_val:
     val_files = np.asarray(val_files)
     val_files = [data_dir + fil for fil in val_files]
-    print("Validation dataset")
-    Back, Back_nod, Fit_Parameters, peak_ind, peak_val, v_real, v_real_no_d, v_real_d, phi, Back_fit = acquire_pixels(val_files,n_patches,max_imgs,f_ran,s,flag_ow,flag_fit,fl_normalize_transient,freqs)
+    print("\nValidation dataset:")
+    Back, Back_nod, gt_real, Fit_Parameters, peak_ind, peak_val, v_real, v_real_no_d, v_real_d, phi, Back_fit = acquire_pixels(val_files, n_patches, max_imgs, f_ran, s, flag_ow, flag_fit, fl_normalize_transient, freqs)
     num_elem = Back.shape[0]
 
-    A_in,phi_in,A_g,phi_g,A_d,phi_d = Aphi_compute(v_real,v_real_no_d,v_real_d)
-    file_val = out_dir + "val_walls_"+str(num_elem)+add_str+".h5"
-    with h5py.File(file_val,'w') as f:
+    A_in, phi_in, A_g, phi_g, A_d, phi_d = Aphi_compute(v_real, v_real_no_d, v_real_d)
+    file_val = out_dir + "val_n" + str(num_elem) + add_str + ".h5"
+    with h5py.File(file_val, 'w') as f:
         dset = f.create_dataset("name", data=datasetname)
         dset = f.create_dataset("transient", data=Back, compression="gzip")
         dset = f.create_dataset("transient_global", data=Back_nod, compression="gzip")
+        dset = f.create_dataset("gt", data=gt_real, compression="gzip")
         dset = f.create_dataset("raw_itof", data=v_real)
         dset = f.create_dataset("global_itof", data=v_real_no_d)
         dset = f.create_dataset("direct_itof", data=v_real_d)
-        dset = f.create_dataset("amplitude_raw",data = A_in)
-        dset = f.create_dataset("phase_raw",data = phi_in)
-        dset = f.create_dataset("amplitude_direct",data = A_d)
-        dset = f.create_dataset("phase_direct",data = phi_d)
-        dset = f.create_dataset("amplitude_global",data = A_g)
-        dset = f.create_dataset("phase_global",data = phi_g)
-        dset = f.create_dataset("freqs",data = freqs)
+        dset = f.create_dataset("amplitude_raw", data=A_in)
+        dset = f.create_dataset("phase_raw", data=phi_in)
+        dset = f.create_dataset("amplitude_direct", data=A_d)
+        dset = f.create_dataset("phase_direct", data=phi_d)
+        dset = f.create_dataset("amplitude_global", data=A_g)
+        dset = f.create_dataset("phase_global", data=phi_g)
+        dset = f.create_dataset("freqs", data=freqs)
         if flag_fit:
-            dset = f.create_dataset("transient_fit",data = Back_fit)
+            dset = f.create_dataset("transient_fit", data=Back_fit)
 
 # TEST IMAGES
 if fl_get_test:
     test_files = np.asarray(test_files)
     test_files = [data_dir + fil for fil in test_files]
-    print("Test dataset")
-    Back, Back_nod,  Fit_Parameters, peak_ind, peak_val, v_real, v_real_no_d, v_real_d, phi, Back_fit = acquire_pixels(test_files,n_patches,max_imgs,f_ran,s,flag_ow,flag_fit,fl_normalize_transient,freqs)
+    print("\nTest dataset:")
+    Back, Back_nod, gt_real, Fit_Parameters, peak_ind, peak_val, v_real, v_real_no_d, v_real_d, phi, Back_fit = acquire_pixels(test_files, n_patches, max_imgs, f_ran, s, flag_ow, flag_fit, fl_normalize_transient, freqs)
     num_elem = Back.shape[0]
 
-    A_in,phi_in,A_g,phi_g,A_d,phi_d = Aphi_compute(v_real,v_real_no_d,v_real_d)
-    file_test = out_dir + "test_walls_"+str(num_elem)+add_str+".h5"
-    with h5py.File(file_test,'w') as f:
+    A_in, phi_in, A_g, phi_g, A_d, phi_d = Aphi_compute(v_real, v_real_no_d, v_real_d)
+    file_test = out_dir + "test_n" + str(num_elem) + add_str + ".h5"
+    with h5py.File(file_test, 'w') as f:
         dset = f.create_dataset("name", data=datasetname)
         dset = f.create_dataset("transient", data=Back, compression="gzip")
         dset = f.create_dataset("transient_global", data=Back_nod, compression="gzip")
+        dset = f.create_dataset("gt", data=gt_real, compression="gzip")
         dset = f.create_dataset("raw_itof", data=v_real)
         dset = f.create_dataset("global_itof", data=v_real_no_d)
         dset = f.create_dataset("direct_itof", data=v_real_d)
-        dset = f.create_dataset("amplitude_raw",data = A_in)
-        dset = f.create_dataset("phase_raw",data = phi_in)
-        dset = f.create_dataset("amplitude_direct",data = A_d)
-        dset = f.create_dataset("phase_direct",data = phi_d)
-        dset = f.create_dataset("amplitude_global",data = A_g)
-        dset = f.create_dataset("phase_global",data = phi_g)
-        dset = f.create_dataset("freqs",data = freqs)
+        dset = f.create_dataset("amplitude_raw", data=A_in)
+        dset = f.create_dataset("phase_raw", data=phi_in)
+        dset = f.create_dataset("amplitude_direct", data=A_d)
+        dset = f.create_dataset("phase_direct", data=phi_d)
+        dset = f.create_dataset("amplitude_global",data=A_g)
+        dset = f.create_dataset("phase_global", data=phi_g)
+        dset = f.create_dataset("freqs", data=freqs)
         if flag_fit:
-            dset = f.create_dataset("transient_fit",data = Back_fit)
+            dset = f.create_dataset("transient_fit", data=Back_fit)
 
-
+end_time = time.time()
+minutes, seconds = divmod(end_time - start_time, 60)
+hours, minutes = divmod(minutes, 60)
+print("\nTotal time to build the whole dataset is %d:%02d:%02d" % (hours, minutes, seconds))
 
 
