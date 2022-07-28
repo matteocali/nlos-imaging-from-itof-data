bd1ba5f75deacb02ad3d6ad6ed6ca4e6ec13ce9d
DECaligM <matteo.caligiuri@sony.com>
2022-07-28 10:09:45 +0200

    Add docstring
    Former-commit-id: e812ed7f40ace0fb4e7189f882df730cd908ade3
    Former-commit-id: 12bc5fdb499923af05445a68f6e46a12e06cbd21 [formerly c7269f85b4589846d88e16197e0cc5e3df011b65] [formerly 32bee3bb3e98d3c1e8699d46abc03b9bbbec9bfe [formerly dd85a8697dd79a9c5e003fd77dff7377bbcb8f34]] [formerly 2f8e5c2c0a44801162e0a264f949f0bf71271bbc [formerly 66756292514502025d59ee21ebd18e1c723f7e53] [formerly 6c8495d54708e69364831a948077002649ea6e50]]
    Former-commit-id: fefc6c1716b148f6c111030dfe529a2b4de1a70b [formerly c73e52f3ec79bffcd82a9f8b056a18f16e8362de] [formerly 0919a8007cf9b8ae8e2f48540d0e131d49a0e4b1]
    Former-commit-id: d443784ec08c21663809392693ebafec3bf9bfbe [formerly 750405d1061b42460de0d9f68b7dc530ec5106a3]
    Former-commit-id: b2776d889bd66db7810e8a90be10c137d1dc1a0b
    Former-commit-id: e4acf4029db84660ceabe13da0a80d4b3afc93ec [formerly ffb8d1b64736db1829e9fa10c9221cd2478ac71d]
    Former-commit-id: 71239117844821a7ea5c9d4ead3a283c5db286d7

--- 2_Implementation/py/src/modules/dataset_func.py
+++ 2_Implementation/py/src/modules/dataset_func.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from random import seed as rnd_seed, sample as rnd_sample
 from lxml import etree as et
 from tqdm import trange, tqdm
-from numpy import nonzero, unique, zeros as np_zeros, where, sum as np_sum, swapaxes
+from numpy import nonzero, unique, zeros as np_zeros, where, sum as np_sum, swapaxes, ndarray
 
 from modules.utilities import create_folder, permute_list, load_list, save_list, blender2mitsuba_coord_mapping, spot_bitmap_gen, read_folders, save_h5, load_h5, read_files
 from modules.transient_handler import transient_loader, compute_distance_map
@@ -392,81 +392,102 @@ def generate_dataset_xml_splitted(tr_rot_list: list, template: Path, folder_path
 
 
 def build_mirror_gt(gt_path: Path, out_path: Path, fov: int, exp_time: float) -> None:
+    """
+    Build the mirror ground truth.
+    Load all the output from mitsuba, put them in standard form. From them extracts:
+    - depth_map (radial)
+    - alpha_map
+    Finally save the obtained data in the out_path folder as a h5 file.
+    :param gt_path: Path to the folder containing the output from mitsuba.
+    :param out_path: Path to the folder where the output will be saved.
+    :param fov: Field of view of the camera.
+    :param exp_time: Exposure time of the camera.
+    """
 
     if not out_path.exists():  # Create out_path if it doesn't exist
         out_path.mkdir(parents=True)
 
-    batches_folder = read_folders(gt_path)
+    batches_folder = read_folders(gt_path)  # Get the list of batches
     data_path = []
     for batch_folder in batches_folder:
-        data_folder = read_folders(batch_folder)
-        data_path = data_path + data_folder
+        data_folder = read_folders(batch_folder)  # Get the list of data in each batch
+        data_path = data_path + data_folder  # Put together (in the same list) all the file present in all the batches
 
-    for file_path in tqdm(data_path, desc="Generating ground truth data"):
-        file_name = str(Path(file_path).name) + "_GT"
-        tr = transient_loader(file_path)
-        d_map = compute_distance_map(tr, fov, exp_time)
-        a_map = np_zeros(d_map.shape)
-        a_map[where(np_sum(tr[:, :, :, -1], axis=0) != 0)] = 1
-        save_h5(file_path=out_path / file_name, data={"depth_map": d_map, "alpha_map": a_map}, fermat=False)
+    for file_path in tqdm(data_path, desc="Generating ground truth data"):  # For each file
+        file_name = str(Path(file_path).name) + "_GT"  # Get the file name and add the suffix
+        tr = transient_loader(file_path)  # Load the data and put them in standard form
+        d_map = compute_distance_map(tr, fov, exp_time)  # Compute the distance map (depth radial map)
+        a_map = np_zeros(d_map.shape)  # Create the alpha map
+        a_map[where(np_sum(tr[:, :, :, -1], axis=0) != 0)] = 1  # Set the alpha map to 1 where there is data in d_map
+        save_h5(file_path=out_path / file_name, data={"depth_map": d_map, "alpha_map": a_map}, fermat=False)  # Save the data in the out_path folder as a h5 file
 
 
 def load_dataset(d_path: Path, out_path: Path) -> None:
+    """
+    Load the dataset and save it in the out_path folder.
+    :param d_path: folder containing the dataset (raw output of mitsuba).
+    :param out_path: folder where the dataset will be saved after processing.
+    """
     if not out_path.exists():  # Create out_path if it doesn't exist
         out_path.mkdir(parents=True)
 
-    batches_folder = read_folders(d_path)
+    batches_folder = read_folders(d_path)  # Get the list of batches
     data_path = []
     for batch_folder in batches_folder:
-        data_folder = read_folders(batch_folder)
-        data_path = data_path + data_folder
+        data_folder = read_folders(batch_folder)  # Get the list of data in each batch
+        data_path = data_path + data_folder  # Put together (in the same list) all the file present in all the batches
 
-    for file_path in tqdm(data_path, desc="Loading dataset"):
-        file_name = str(Path(file_path).name)
-        tr = transient_loader(file_path)[:, :, :, 1]
-        save_h5(file_path=out_path / file_name, data={"data": tr}, fermat=True)
+    for file_path in tqdm(data_path, desc="Loading dataset"):  # For each file
+        file_name = str(Path(file_path).name)  # Get the file name
+        tr = transient_loader(file_path)[:, :, :, 1]  # Load the data and put them in standard form (only green channel is considered)
+        save_h5(file_path=out_path / file_name, data={"data": tr}, fermat=True)  # Save the data in the out_path folder as a h5 file
 
 
 def fuse_dt_gt(d_path: Path, gt_path: Path, out_path: Path) -> None:
+    """
+    Fuse the dataset and the ground truth together in the same h5 file.
+    :param d_path: folder containing the dataset (already processed and in h5 form).
+    :param gt_path: folder containing the ground truth (already processed and in h5 form).
+    :param out_path: folder where the fused dataset will be saved.
+    """
+
     if not out_path.exists():  # Create out_path if it doesn't exist
         out_path.mkdir(parents=True)
 
-    d_files_name = [Path(i).name for i in read_files(d_path, "h5")]
-    gt_files_name = [Path(i).name for i in read_files(gt_path, "h5")]
+    d_files_name = [Path(i).name for i in read_files(d_path, "h5")]  # Get the list of files in d_path (only name)
+    gt_files_name = [Path(i).name for i in read_files(gt_path, "h5")]  # Get the list of files in gt_path (only name)
 
     if len(d_files_name) != len(gt_files_name):
-        raise ValueError("The number of files in the dataset and the ground truth folder are different")
+        raise ValueError("The number of files in the dataset and the ground truth folder are different")  # Raise an error if the number of files is different
 
-    for d_name in tqdm(d_files_name[60:], desc="Fusing dataset and ground truth"):
+    for d_name in tqdm(d_files_name[60:], desc="Fusing dataset and ground truth"):  # For each file in d_path
         d_name_shortened = d_name.replace("cam_pos_(1.5_-1_1.65)_cam_rot_(90_0_50)_", "")[:-3]  # Remove the cam_pos and cam_rot from the file name since I'm considering just the fixed camera position
-        obj_name = d_name_shortened[(d_name_shortened.find("nlos") + 5):(d_name_shortened.find("obj") - 1)]
-        obj_pos = d_name_shortened[(d_name_shortened.find("obj_pos") + 9):(d_name_shortened.find("obj_rot") - 2)]
-        obj_rot = d_name_shortened[(d_name_shortened.find("obj_rot") + 9):(-1)]
-        if d_name_shortened.find("+") == -1:
-            obj_pos = d_name_shortened[(d_name_shortened.find("obj_pos") + 9):(d_name_shortened.find("obj_rot") - 2)]
-            obj_rot = d_name_shortened[(d_name_shortened.find("obj_rot") + 9):(-1)]
-            obj_pos = [float(i) for i in obj_pos.split("_")]
-            obj_pos = [round(obj_pos[i] - elm, 1) for i, elm in enumerate([1.5, 1.0, 1.65])]
-            obj_rot = [int(float(i)) for i in obj_rot.split("_")]
-            gt_name = f"transient_nlos_{obj_name}_tr({obj_pos[0]}_{obj_pos[1]}_{obj_pos[2]})_rot({obj_rot[0]}_{obj_rot[1]}_{obj_rot[2]})_GT.h5"
-        else:
-            obj_name1 = obj_name.split("+")[0]
-            obj_name2 = obj_name.split("+")[1]
-            obj_pos1 = obj_pos.split(")_(")[0]
-            obj_pos2 = obj_pos.split(")_(")[1]
-            obj_pos1_raw = [float(i) for i in obj_pos1.split("_")]
-            obj_pos2_raw = [float(i) for i in obj_pos2.split("_")]
-            obj_pos1 = [round(obj_pos1_raw[i] - elm, 1) for i, elm in enumerate([1.5, 1.0, 1.65])]
-            obj_pos2 = [round(obj_pos2_raw[i] - elm, 1) for i, elm in enumerate([1.5, 1.0, 1.65])]
-            obj_rot1 = obj_rot.split(")_(")[0]
-            obj_rot2 = obj_rot.split(")_(")[1]
-            obj_rot1 = [int(float(i)) for i in obj_rot1.split("_")]
-            obj_rot2 = [float(i) for i in obj_rot2.split("_")]
-            gt_name = f"transient_nlos_{obj_name1}_tr({obj_pos1[0]}_{obj_pos1[1]}_{obj_pos1[2]})_rot({obj_rot1[0]}_{obj_rot1[1]}_{obj_rot1[2]})_{obj_name2}_tr({obj_pos2[0]}_{obj_pos2[1]}_{obj_pos2[2]})_rot({obj_rot2[0]}_{obj_rot2[1]}_{obj_rot2[2]})_GT.h5"
+        obj_name = d_name_shortened[(d_name_shortened.find("nlos") + 5):(d_name_shortened.find("obj") - 1)]  # Get the object name
+        obj_pos = d_name_shortened[(d_name_shortened.find("obj_pos") + 9):(d_name_shortened.find("obj_rot") - 2)]  # Get the object position (as string)
+        obj_rot = d_name_shortened[(d_name_shortened.find("obj_rot") + 9):(-1)]  # Get the object rotation (as string)
+        if d_name_shortened.find("+") == -1:  # If the considered file is characterized by only one object
+            obj_pos = [float(i) for i in obj_pos.split("_")]  # Convert the object position to float
+            obj_pos = [round(obj_pos[i] - elm, 1) for i, elm in enumerate([1.5, 1.0, 1.65])]  # Remove the offset from the object position (get the object translation)
+            obj_rot = [int(float(i)) for i in obj_rot.split("_")]  # Convert the object rotation to int
+            gt_name = f"transient_nlos_{obj_name}_tr({obj_pos[0]}_{obj_pos[1]}_{obj_pos[2]})_rot({obj_rot[0]}_{obj_rot[1]}_{obj_rot[2]})_GT.h5"  # Compose the name of the gt file starting from the one of the dataset
+        else:  # If the considered file is characterized by two objects
+            obj_name1 = obj_name.split("+")[0]  # Get the first object name
+            obj_name2 = obj_name.split("+")[1]  # Get the second object name
+            obj_pos1 = obj_pos.split(")_(")[0]  # Get the first object position
+            obj_pos2 = obj_pos.split(")_(")[1]  # Get the second object position
+            obj_pos1_raw = [float(i) for i in obj_pos1.split("_")]  # Convert the first object position to float
+            obj_pos2_raw = [float(i) for i in obj_pos2.split("_")]  # Convert the second object position to float
+            obj_pos1 = [round(obj_pos1_raw[i] - elm, 1) for i, elm in enumerate([1.5, 1.0, 1.65])]  # Remove the offset from the first object position (get the object translation)
+            obj_pos2 = [round(obj_pos2_raw[i] - elm, 1) for i, elm in enumerate([1.5, 1.0, 1.65])]  # Remove the offset from the second object position (get the object translation)
+            obj_rot1 = obj_rot.split(")_(")[0]  # Get the first object rotation
+            obj_rot2 = obj_rot.split(")_(")[1]  # Get the second object rotation
+            obj_rot1 = [int(float(i)) for i in obj_rot1.split("_")]  # Convert the first object rotation to int
+            obj_rot2 = [float(i) for i in obj_rot2.split("_")]  # Convert the second object rotation to float
+            gt_name = f"transient_nlos_{obj_name1}_tr({obj_pos1[0]}_{obj_pos1[1]}_{obj_pos1[2]})_rot({obj_rot1[0]}_{obj_rot1[1]}_{obj_rot1[2]})_{obj_name2}_tr({obj_pos2[0]}_{obj_pos2[1]}_{obj_pos2[2]})_rot({obj_rot2[0]}_{obj_rot2[1]}_{obj_rot2[2]})_GT.h5"  # Compose the name of the gt file starting from the one of the dataset
 
-        if gt_name not in gt_files_name:
+        if gt_name not in gt_files_name:  # If the gt file doesn't exist
             # noinspection PyUnboundLocalVariable
-            obj_pos1 = [round(obj_pos1_raw[i] - elm, 1) for i, elm in enumerate([1.5, 1.0, 1.55])]
+            obj_pos1 = [round(obj_pos1_raw[i] - elm, 1) for i, elm in enumerate([1.5, 1.0, 1.55])]  # Change offset to remove and check again if the gt_file exists (this is due ti an error in the labeling of the dataset files)
             # noinspection PyUnboundLocalVariable
             gt_name = f"transient_nlos_{obj_name1}_tr({obj_pos1[0]}_{obj_pos1[1]}_{obj_pos1[2]})_rot({obj_rot1[0]}_{obj_rot1[1]}_{obj_rot1[2]})_{obj_name2}_tr({obj_pos2[0]}_{obj_pos2[1]}_{obj_pos2[2]})_rot({obj_rot2[0]}_{obj_rot2[1]}_{obj_rot2[2]})_GT.h5"
             if gt_name not in gt_files_name:
@@ -480,17 +501,20 @@ def fuse_dt_gt(d_path: Path, gt_path: Path, out_path: Path) -> None:
                     # noinspection PyUnboundLocalVariable
                     obj_pos1 = [round(obj_pos1_raw[i] - elm, 1) for i, elm in enumerate([1.5, 1.0, 1.55])]
                     # noinspection PyUnboundLocalVariable
-                    obj_pos2 = [round(obj_pos2_raw[i] - elm, 1) for i, elm in enumerate([1.5, 1.0, 1.55])]
-                    # noinspection PyUnboundLocalVariable
                     gt_name = f"transient_nlos_{obj_name1}_tr({obj_pos1[0]}_{obj_pos1[1]}_{obj_pos1[2]})_rot({obj_rot1[0]}_{obj_rot1[1]}_{obj_rot1[2]})_{obj_name2}_tr({obj_pos2[0]}_{obj_pos2[1]}_{obj_pos2[2]})_rot({obj_rot2[0]}_{obj_rot2[1]}_{obj_rot2[2]})_GT.h5"
                     if gt_name not in gt_files_name:
-                        raise ValueError("The ground truth file is missing")
+                        raise ValueError("The ground truth file is missing")  # If the gt file doesn't exist, raise an error
 
-        d_file = d_path / d_name
-        gt_file = gt_path / gt_name
-        d = load_h5(d_file)
-        gt = load_h5(gt_file)
+        d_file = d_path / d_name  # Compose the path of the dataset file
+        gt_file = gt_path / gt_name  # Compose the path of the gt file
+        d = load_h5(d_file)  # Load the dataset file
+        gt = load_h5(gt_file)  # Load the gt file
 
-        file_name = d_name_shortened.replace("-", "n").replace("(", "").replace(")", "").replace(".", "dot")
+        file_name = d_name_shortened.replace("-", "n").replace("(", "").replace(")", "").replace(".", "dot")  # Compose the name of the file
 
-        save_h5(file_path=out_path / file_name, data={"data": d["data"], "depth_map": swapaxes(gt["depth_map"], 0, 1), "alpha_map": swapaxes(gt["alpha_map"], 0, 1)}, fermat=False)
+        save_h5(file_path=out_path / file_name, data={"data": d["data"], "depth_map": swapaxes(gt["depth_map"], 0, 1), "alpha_map": swapaxes(gt["alpha_map"], 0, 1)}, fermat=False)  # Save the file
+
+
+def build_point_cloud(data: ndarray, fov: int, img_size: tuple[int, int], alpha: ndarray = None):
+    if alpha is not None:
+        pass
