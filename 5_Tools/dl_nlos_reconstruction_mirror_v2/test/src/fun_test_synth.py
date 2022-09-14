Fixed the MAE computation on the alpha map
Former-commit-id: ef370b5aaec5dd7e523271d912f5e0fa902b607f
Former-commit-id: 9bc48d77ad42c440af4cc40e145e1caf9f0af5b0 [formerly 35dfb639641a0a629f61299800560cda0ab7e87c] [formerly a16e2843a507726a859acc2520ea3590adb70546 [formerly 0fe577a57a55f43ac05ad532dadb3c63ad2511f1]] [formerly e03ee20c41c176effe7d8041b0a4a1f39bc4b5ed [formerly e2346b23e1158c11b13db6f86aefee505176ebcb] [formerly 565ec57d7ccb04bf03f4eb074fda5f724d8cf0f9]]
Former-commit-id: bae440435b17d75952237ddd6a5d2160a9bdc78d [formerly 71e8119e3bda63698ed90caad88bb7e20d6fc7a2] [formerly 9fa7b208dd910e80b8fb3919ff6016c8dce9d4f7]
Former-commit-id: cd6279ca8b38c2ad6176495c1bfbffd668f8aea0 [formerly 81c9c68902f90d57aa6bc8991f3b045fa1e10d3b]
Former-commit-id: 9de6c051ea7a7aa4dfd177cea9e8cbfe5d46d74a
Former-commit-id: 6af41d1f06a633c81d4bede6f6c4a4ba2cfc487e [formerly db1eff37c472a00ed3c5062487276cd0ab27dea9]
Former-commit-id: cfc65d1477614cfe52d3f9b6e2abc905a6fc95cf

5_Tools/dl_nlos_reconstruction_mirror_v2/test/src/fun_test_synth.py
@@ -24,9 +24,9 @@ def plot_results(out_path, name, gt_depth_data, gt_alpha_data, pred_depth, pred_
     alpha_mae_bkg = np.sum(np.abs(pred_alpha_masked_zeros - np.zeros(gt_alpha_data.shape, dtype=np.float32))) / num_zeros
     alpha_mae = np.sum(alpha_mae_obj + alpha_mae_bkg) / 2
     # MAE on the depth map
-    pred_depth_masked_ones = pred_depth * gt_alpha_data
+    pred_depth_masked_ones = pred_depth_masked * gt_alpha_data
     depth_mae_obj = np.sum(np.abs(pred_depth_masked_ones - gt_depth_data)) / num_ones
-    pred_depth_masked_zeros = pred_depth * (1 - gt_alpha_data)
+    pred_depth_masked_zeros = pred_depth_masked * (1 - gt_alpha_data)
     depth_mae_bkg = np.sum(np.abs(pred_depth_masked_zeros - np.zeros(gt_alpha_data.shape, dtype=np.float32))) / num_zeros
     depth_mae = np.sum(depth_mae_obj + depth_mae_bkg) / 2
 