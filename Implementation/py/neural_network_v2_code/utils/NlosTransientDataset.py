Removed rotation and translation from the transformation and fix the gaussian noise to not modify gt
Former-commit-id: e56bdc05b50abe70f7938cef08ef74a05976eb9a
Former-commit-id: 36cb9e6ada4af08082acab0fa68f6209d0294ec5 [formerly 5de6cb619f12319662dcd2552910c0aeedbe0919] [formerly 38e818268e28efff101ee7626abd841d71380976 [formerly b1566609169bb5b985a48b20831f74759deb444f]] [formerly 9fe659663c79e079ed48bf5d70d28b50a1f22e56 [formerly 24ef1a23be5f011552355845d62e7a1a9a51367c] [formerly c203846c193ba225b56b4f0776b3321407a3a4aa]]
Former-commit-id: 679fa4b113fb34e1907a26fa2d601959000acb86 [formerly 782c1e52c63fa233382ef958ba707c800d4a2b8f] [formerly 3d67d049c242e9045f2aa25494249d60b8044dfb]
Former-commit-id: dccb507685150742eb7b1da412f5cdc81916a6b7 [formerly 1029e947726b2d399b09c45a2b0e60ea58821671]
Former-commit-id: f85de07cfee82e83fec5d4702aa4bc5eb5cee29e
Former-commit-id: f0485cdacafdea3b7abb538e3f4a2563b87ccbe9 [formerly 23c3d8480eed830725600877c041b4afd861721f]
Former-commit-id: 8f6b5da8f1cc47fef7d637885495dd0a9f10d8a8

Implementation/py/neural_network_v2_code/utils/NlosTransientDataset.py
@@ -142,11 +142,11 @@ class NlosTransientDataset(Dataset):
 
         # Define the transforms to apply to the dataset
         transforms = {
-            "random rotate": CT.RandomRotation(degrees=180, interpolation=T.InterpolationMode.NEAREST, fill=float("inf")),
-            "random translate": CT.RandomAffine(degrees=0, translate=(0.2, 0.2), interpolation=T.InterpolationMode.NEAREST, fill=float("inf")),
+            #"random rotate": CT.RandomRotation(degrees=180, interpolation=T.InterpolationMode.NEAREST, fill=float("inf")),
+            #"random translate": CT.RandomAffine(degrees=0, translate=(0.2, 0.2), interpolation=T.InterpolationMode.NEAREST, fill=float("inf")),
             "random hflip": T.RandomHorizontalFlip(p=1.0),
             "random vflip": T.RandomVerticalFlip(p=1.0),
-            "random oise": CT.AddGaussianNoise(mean=0.0, std=1.0)
+            "random noise": CT.AddGaussianNoise(mean=0.0, std=1.0)
         }
         
         # Sample a random batch of elements for each transform
@@ -168,8 +168,9 @@ class NlosTransientDataset(Dataset):
 
                 # Extract the data from the transformed tensor
                 itof_data = data[0, 0:6, ...].unsqueeze(0)
-                gt_depth = data[0, 6, ...].unsqueeze(0)
-                gt_mask = data[0, 7, ...].unsqueeze(0)
+                if key != "random noise":  # The noise transform does not change the ground truth
+                    gt_depth = data[0, 6, ...].unsqueeze(0)
+                    gt_mask = data[0, 7, ...].unsqueeze(0)
 
                 # Update the dataset
                 self.itof_data = torch.cat((self.itof_data, itof_data), dim=0)