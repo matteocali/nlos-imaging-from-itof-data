bb4fee9bf7d10cff1d2a7f0dc8d8e46749d4aa3f
Matteo Caligiuri <matteo.caligiuri@dei.unipd.it>
2023-05-16 11:24:37 +0200

    Update the weight loss md file
    Former-commit-id: 4e31a682301661b5365d8739f280aedf226a1024
    Former-commit-id: 8e3a82c2005d46b4e516eaa0cc8a90d75a83a19a [formerly 6560df9369df2461339a7996409d525a4c2b7f16] [formerly 66a3970c09c15131b0faa0a6466d56b35ea93b4a [formerly d69b707657c59b3158176c318c8d9c08ff60ace1]] [formerly 9c689c54cb0a680d6ec03817a98dd6f7c2df3610 [formerly f9fde9e8ad685c40834e380ed1e8ed574a71911b] [formerly 860840a3df69bc5498545eaa4b141495a55b2cd7]]
    Former-commit-id: bd916c01fa35c52c2c50a931e29ce9aa63ce1cea [formerly 50c3c3fbbf85c5128ccd3a79223c3ad04e71d970] [formerly a7629e3ccf2116e8249a8231e74e973e15ea2704]
    Former-commit-id: 0b794be9b4ebcc3e472d3ec1181cd06f35ed7553 [formerly e799413e704db50d50d84edb73dbdfcbfa1c4903]
    Former-commit-id: aa015c4eadda11c69c20cf02b90ae1dd550b7e8b
    Former-commit-id: 83f6ecf1d456c1a761421ac65357b1d2fdff7594 [formerly 3c5e2e514ef114a7f0d56e58542a427b2309823a]
    Former-commit-id: a9d5fc6a8458a72c00f9b547367396535630221c

--- Implementation/py/neural_network_v2_code/additional_script/multi_test.sh
+++ Implementation/py/neural_network_v2_code/additional_script/multi_test.sh
@@ -1,56 +1,58 @@
 #!/bin/bash
 
+cd ..
+
 pydir=~/miniconda3/envs/pytorch_nn/bin
 
-$pydir/python3 ../test.py\
+$pydir/python3 test.py\
     --dts-name="fixed_camera_full_add_layer"\
-    --model="nlos_nn_v2_itof_out_add_layer_aug_scale_0.14_iou_1.0_small_lr_big_net_model_lr_5e-05_ochannel_16_l_0.0_addlayers_0_aug_403"\
-    --encoder-channels="40, 80, 160, 320, 640"\
+    --model="nlos_nn_v2_itof_out_add_layer_aug_test_weight_loss_0.2_model_lr_0.0001_ochannel_16_l_0.0_addlayers_0_aug_403"\
+    --encoder-channels="32, 64, 128, 256, 512"\
     --n-out-channels="16"\
     --additional-layers="0"\
     --bg-value="0"\
 &\
-$pydir/python3 ../test.py\
+$pydir/python3 test.py\
     --dts-name="fixed_camera_full_add_layer"\
-    --model="nlos_nn_v2_itof_out_add_layer_aug_scale_0.14_iou_1.0_small_lr_double_net_model_lr_5e-05_ochannel_16_l_0.0_addlayers_0_aug_403"\
-    --encoder-channels="64, 128, 256, 512, 1024"\
+    --model="nlos_nn_v2_itof_out_add_layer_aug_test_weight_loss_0.25_model_lr_0.0001_ochannel_16_l_0.0_addlayers_0_aug_403"\
+    --encoder-channels="32, 64, 128, 256, 512"\
     --n-out-channels="16"\
     --additional-layers="0"\
     --bg-value="0"
 #&\
-#$pydir/python3 ../test.py\
+#$pydir/python3 test.py\
 #    --dts-name="fixed_camera_full_add_layer"\
-#    --model="tmp_itof_out_add_layer_aug_0.14_0.14_iou_1.0_BCE_model_lr_0.0001_ochannel_16_l_0.0_addlayers_0_aug_403"\
+#    --model="nlos_nn_v2_itof_out_add_layer_aug_scale_0.111_iou_0.0_model_lr_0.0001_ochannel_16_l_0.0_addlayers_0_aug_403"\
 #    --encoder-channels="32, 64, 128, 256, 512"\
 #    --n-out-channels="16"\
 #    --additional-layers="0"\
 #    --bg-value="0"\
 #&\
-#$pydir/python3 ../test.py\
+#$pydir/python3 test.py\
 #    --dts-name="fixed_camera_full_add_layer"\
-#    --model="tmp_itof_out_add_layer_aug_0.14_0.14_iou_1.0_itof_lovasz_model_lr_0.0001_ochannel_16_l_0.0_addlayers_0_aug_403"\
+#    --model="nlos_nn_v2_itof_out_add_layer_aug_scale_0.125_iou_0.0_model_lr_0.0001_ochannel_16_l_0.0_addlayers_0_aug_403"\
 #    --encoder-channels="32, 64, 128, 256, 512"\
 #    --n-out-channels="16"\
 #    --additional-layers="0"\
 #    --bg-value="0"\
 #&\
-#$pydir/python3 ../test.py\
+#$pydir/python3 test.py\
 #    --dts-name="fixed_camera_full_add_layer"\
-#    --model="tmp_itof_out_add_layer_aug_0.14_0.14_iou_1.0_softmax_lovasz_model_lr_0.0001_ochannel_16_l_0.0_addlayers_0_aug_403"\
+#    --model="nlos_nn_v2_itof_out_add_layer_aug_scale_0.167_iou_0.0_model_lr_0.0001_ochannel_16_l_0.0_addlayers_0_aug_403"\
 #    --encoder-channels="32, 64, 128, 256, 512"\
 #    --n-out-channels="16"\
 #    --additional-layers="0"\
 #    --bg-value="0"\
 #&\
-#$pydir/python3 ../test.py\
+#$pydir/python3 test.py\
 #    --dts-name="fixed_camera_full_add_layer"\
-#    --model="nlos_nn_v2_itof_out_add_layer_aug_scale_0.14_iou_3.0_model_lr_0.0001_ochannel_16_l_0.0_addlayers_0_aug_403"\
+#    --model="nlos_nn_v2_itof_out_add_layer_aug_scale_10_iou_0.0_model_lr_0.0001_ochannel_16_l_0.0_addlayers_0_aug_403"\
 #    --encoder-channels="32, 64, 128, 256, 512"\
 #    --n-out-channels="16"\
 #    --additional-layers="0"\
 #    --bg-value="0"\
 #&\
-#$pydir/python3 ../test.py\
+#$pydir/python3 test.py\
 #    --dts-name="fixed_camera_full_add_layer"\
 #    --model="nlos_nn_v2_itof_out_add_layer_aug_scale_0.14_iou_5.0_model_lr_0.0001_ochannel_16_l_0.0_addlayers_0_aug_403"\
 #    --encoder-channels="32, 64, 128, 256, 512"\
@@ -58,7 +60,7 @@ $pydir/python3 ../test.py\
 #    --additional-layers="0"\
 #    --bg-value="0"\
 #&\
-#$pydir/python3 ../test.py\
+#$pydir/python3 test.py\
 #    --dts-name="fixed_camera_full_add_layer"\
 #    --model="nlos_nn_v2_itof_out_add_layer_aug_l_5.0_l2_0_loss_grad-mae_dts_fixed_full_model_lr_0.0001_ochannel_16_l_5.0_addlayers_0_aug_403"\
 #    --lr="0.0001"\
@@ -67,7 +69,7 @@ $pydir/python3 ../test.py\
 #    --additional-layers="0"\
 #    --bg-value="0"\
 #&\
-#$pydir/python3 ../test.py\
+#$pydir/python3 test.py\
 #    --dts-name="fixed_camera_full_add_layer"\
 #    --model="nlos_nn_v2_itof_out_add_layer_aug_l_5.0_l2_0_loss_grad-mse_dts_fixed_full_model_lr_0.0001_ochannel_16_l_5.0_addlayers_0_aug_403"\
 #    --lr="0.0001"\
