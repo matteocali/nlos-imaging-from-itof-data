Corrected the computation of the IoU in order to be the corrected mIoU. Performed modification in both train and test. Added a function in utils.utils and a loss in Custom Losses
Former-commit-id: 73853b5437eb869621fe2aa1ced42393c841c520
Former-commit-id: 905abf9af846ad910355b17a4e58e1f51469c4a9 [formerly ad63aa5318ada29326b0568337323e1291b78deb] [formerly 5b4b16995586a8ec0721bc18c2e34fbca3d9eded [formerly 141369b284b290e0b727d8c110b743486d8d310e]] [formerly ffd61f44a3bd60d800646ce37ca3841c4f4d8bcb [formerly 9cd9ccd092c5c84b587d45b4713fa9cf0c27dc90] [formerly 2c994244b865d53078d8718cc7ebad0b6741aa51]]
Former-commit-id: 0f36e0f3f3336f2098cbd4ebb21529288f815696 [formerly 9848c041bd051a893836f87f383df71db5513fb8] [formerly 4b16586e50f1c93bd9deb0364df4ba39b5fbf59a]
Former-commit-id: c5e03f731f507079a8e8e039afb4052a64f43902 [formerly 59683235abd58f461d3e60eeb503a2130a2f475a]
Former-commit-id: a5367c8a9ed24da3648397530b37040e60954cf2
Former-commit-id: 1574544cdfa41fef632e7dd259688a1a7a6f1c43 [formerly 62d11133a9dfc610bcf2b9e810cacd20e7a5f4e2]
Former-commit-id: a47a0792ae4287d2fe9e2b4a08722ce9a95374d0

Implementation/py/neural_network_v2_code/utils/test_function.py
@@ -4,7 +4,7 @@ from torch import nn
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 from pathlib import Path
-from torchmetrics.functional.classification.jaccard import binary_jaccard_index
+from utils.utils import mean_intersection_over_union as miou
 from utils.utils import save_test_plots_itof, depth_radial2cartesian, hfov2focal, itof2depth
 
 
@@ -54,9 +54,7 @@ def test(net: nn.Module, data_loader: DataLoader, loss_fn: torch.nn.Module, devi
             depth_loss = loss_fn(depth, gt_depth)  # Compute the loss over the depth
 
             # Compute the mean intersection over union on the depth
-            iou_obj = binary_jaccard_index(torch.where(depth > 0, 1, 0).to("cpu"), torch.where(gt_depth > 0, 1, 0).to("cpu") ).item()  # type: ignore
-            iou_bg = binary_jaccard_index(torch.where(depth == 0, 1, 0).to("cpu"), torch.where(gt_depth == 0, 1, 0).to("cpu") ).item()  # type: ignore
-            iou = (iou_obj + iou_bg) / 2
+            iou = miou(depth.to("cpu"), gt_depth.to("cpu"), 0).item()  # type: ignore
 
             # Update the loss list
             epoch_loss.append(depth_loss.to("cpu").item())

Implementation/py/neural_network_v2_code/utils/train_functions.py
@@ -13,11 +13,10 @@ from torch.optim import Optimizer
 from pathlib import Path
 from torch.utils.tensorboard.writer import SummaryWriter
 from torchmetrics import StructuralSimilarityIndexMeasure as SSIM
-from torchmetrics.functional.classification.jaccard import binary_jaccard_index
-from utils.utils import format_time, generate_fig
+from utils.utils import format_time, generate_fig, itof2depth
+from utils.CustomLosses import BinaryMeanIntersectionOverUnion as miou
 from utils.lovasz_losses import lovasz_hinge
 from utils.EarlyStopping import EarlyStopping
-from utils.utils import itof2depth, update_lr
 from utils.SobelGradient import SobelGrad
 
 
@@ -56,19 +55,22 @@ def compute_loss_itof(itof: torch.Tensor, gt: torch.Tensor, gt_depth: torch.Tens
         # Compute the ssim loss
         second_loss = 1 - add_loss(depth.unsqueeze(0), gt_depth.unsqueeze(0))  # type: ignore
     elif isinstance(add_loss, torch.nn.Module):
-        # Compute the gradient of the prediction and gt
-        # grad_itof = torch.stack(
-        #     (torch.gradient(clean_itof[:, 0, ...], dim=1, edge_order= 2)[0], 
-        #     torch.gradient(clean_itof[:, 0, ...], dim=2, edge_order= 2)[0],
-        #     torch.gradient(clean_itof[:, 1, ...], dim=1, edge_order= 2)[0], 
-        #     torch.gradient(clean_itof[:, 1, ...], dim=2, edge_order= 2)[0]), 
-        #     dim=1)
-        # grad_gt = torch.stack(
-        #     (torch.gradient(gt[:, 0, ...], dim=1, edge_order= 2)[0], 
-        #     torch.gradient(gt[:, 0, ...], dim=2, edge_order= 2)[0],
-        #     torch.gradient(gt[:, 1, ...], dim=1, edge_order= 2)[0], 
-        #     torch.gradient(gt[:, 1, ...], dim=2, edge_order= 2)[0]), 
-        #     dim=1)
+        # The followig commented grad computation should be remove since is not precise as the Sobel operator
+        """
+        Compute the gradient of the prediction and gt
+        grad_itof = torch.stack(
+            (torch.gradient(clean_itof[:, 0, ...], dim=1, edge_order= 2)[0], 
+            torch.gradient(clean_itof[:, 0, ...], dim=2, edge_order= 2)[0],
+            torch.gradient(clean_itof[:, 1, ...], dim=1, edge_order= 2)[0], 
+            torch.gradient(clean_itof[:, 1, ...], dim=2, edge_order= 2)[0]), 
+            dim=1)
+        grad_gt = torch.stack(
+            (torch.gradient(gt[:, 0, ...], dim=1, edge_order= 2)[0], 
+            torch.gradient(gt[:, 0, ...], dim=2, edge_order= 2)[0],
+            torch.gradient(gt[:, 1, ...], dim=1, edge_order= 2)[0], 
+            torch.gradient(gt[:, 1, ...], dim=2, edge_order= 2)[0]), 
+            dim=1)
+        """
 
         # Initialize the Sobel gradient operator with window size 7
         grad = SobelGrad(window_size=7)
@@ -88,7 +90,7 @@ def compute_loss_itof(itof: torch.Tensor, gt: torch.Tensor, gt_depth: torch.Tens
 
     # Compute the Intersection over Union loss (only if l2 is not 0)
     if l2 != 0:
-        iou_loss = 1 - binary_jaccard_index(torch.where(depth > 0, 1, 0), torch.where(gt_depth > 0, 1, 0)).item()  # type: ignore
+        iou_loss = miou()(depth, gt_depth, 0)  # type: ignore
         # iou_loss = lovasz_hinge(torch.where(depth > 0, 1, 0), torch.where(gt_depth > 0, 1, 0)).item() # type: ignore
     else:
         iou_loss = 0
@@ -258,7 +260,7 @@ def train(attempt_name: str, net: torch.nn.Module, train_loader: DataLoader, val
 
     # Initialize the early stopping
     early_stopping = EarlyStopping(
-        tollerance=10, min_delta=0.015, save_path=save_path, net=net)
+        tollerance=150, min_delta=0.015, save_path=save_path, net=net)
     
     # Initialize the chosed additional loss
     if add_loss == "ssim":