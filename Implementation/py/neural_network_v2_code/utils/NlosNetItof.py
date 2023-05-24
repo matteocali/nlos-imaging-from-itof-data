Correct the implementation of the Straight Through Estimator and add a sigmoid in the grad computation
Former-commit-id: c5aaf65eea44de404fe70e4e40e7885d820b4bcb
Former-commit-id: 966ab5fdbf90c3b455cf74b6304a6948e075a9d0 [formerly bbb7bc6a5ee2845f74b85e6678207f6cbbc22d31] [formerly b7754e1e524c899cd8e44a130b19422a344f1b4e [formerly 6b04f2134553885170f518c7f52e228b50ff0101]] [formerly 5ec4b2f94806f3524af4bfde3865ee673356c859 [formerly af8ae887f3dfe750202edae82a1b3f72408bd5ff] [formerly d3aceae8831e5b3628702d106f5a3e6c12b2f011]]
Former-commit-id: fff917aaffb59ea0de99abe82a5524d01eec7cda [formerly b6b0c2a0c02fb1366150c207124e002036003c34] [formerly 26e3b8b79f2ab62094a556e6b39eaa44272b8ae1]
Former-commit-id: 03493436faad79cf9dda72694e628e346b83e1a4 [formerly 1632eb0931cc4406c59856fb9e72ba6952a2b0f1]
Former-commit-id: d14d5c616e97da495aff9d27f69f82bf743f496c
Former-commit-id: 27186760ff17d666f9b4e4c72ad75d3d124d59c5 [formerly d15af3a05252ccc25b6af16881d06628388c67f5]
Former-commit-id: 3b52dd9b3e5e7a0e040d3f9e7559a2b5cbb74591

Implementation/py/neural_network_v2_code/utils/NlosNetItof.py
@@ -2,7 +2,7 @@ import torch
 import torchvision
 from torch import nn
 from math import sqrt
-from torch.autograd import Function
+from utils.StraightThroughEstimators import StraightThroughEstimator
 
 
 class Block(nn.Module):
@@ -145,74 +145,6 @@ class FinalConv(nn.Module):
         return x
 
 
-class StraightThroughClean(Function):
-    """
-    Straight through estimator
-    Clean the iToF data, everything below 0.05 is set to 0
-    """
-
-    @staticmethod
-    def forward(ctx, x: torch.Tensor) -> torch.Tensor:
-        """
-        Forward pass
-        param:
-            - ctx: context
-            - x: input tensor
-        return:
-            - output tensor
-        """
-
-        return torch.where(abs(x) < 0.05, 0, x)  # Clean the itof data
-    
-    @staticmethod
-    def backward(ctx, grad_out) -> torch.Tensor:
-        """
-        Backward pass
-        param:
-            - ctx: context
-            - grad_out: gradient output
-        return:
-            - output tensor
-        """
-
-        return grad_out
-    
-
-class StraightThroughHardThreshold(Function):
-    """
-    Straight through estimator
-    Compute the hard threshold of the depth data
-    """
-
-    @staticmethod
-    def forward(ctx, x: torch.Tensor) -> torch.Tensor:
-        """
-        Forward pass
-        param:
-            - ctx: context
-            - x: input tensor
-        return:
-            - output tensor
-        """
-
-        from utils.utils import itof2depth
-        depth = itof2depth(x, 20e06)
-        return torch.where(depth == 0, 0, 1)
-    
-    @staticmethod
-    def backward(ctx, grad_out) -> torch.Tensor:
-        """
-        Backward pass
-        param:
-            - ctx: context
-            - grad_out: gradient output
-        return:
-            - output tensor
-        """
-
-        return grad_out
-
-
 class NlosNetItof(nn.Module):
 
     def __init__(self, enc_channels: tuple = (6, 64, 128, 256, 512, 1024), dec_channels: tuple = (1024, 512, 256, 128, 64), pad: int = 1, num_class: int = 1, additional_cnn_layers: int = 0) -> None:
@@ -239,8 +171,8 @@ class NlosNetItof(nn.Module):
         self.itof_estiamtor = FinalConv(chs=tuple(chs), additional_layers=additional_cnn_layers)  # Initialize the itof data estimator
 
         # Initialize the straight through estimators
-        self.st_clean = StraightThroughClean()
-        self.st_hard = StraightThroughHardThreshold()
+        self.st_clean = StraightThroughEstimator(task="clean")
+        self.st_hard = StraightThroughEstimator(task="threshold")
 
 
     def forward(self, x: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
@@ -262,7 +194,7 @@ class NlosNetItof(nn.Module):
         itof = self.itof_estiamtor(out)
 
         # Compute the related depthmap and return a hard mask
-        clean_itof = self.st_clean.apply(itof)
-        depth = self.st_hard.apply(clean_itof)
+        clean_itof = self.st_clean(itof)
+        depth = self.st_hard(clean_itof)
 
         return itof.squeeze(1), depth.squeeze(1)