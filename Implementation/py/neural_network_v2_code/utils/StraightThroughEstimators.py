b9541378b278b2c8c30fb41bbad049ca9e6246a5
Matteo Caligiuri <matteo.caligiuri@dei.unipd.it>
2023-05-24 14:58:05 +0200

    In the Straight Throught file moved the import statement on the top and add an alternative implementation of the STE: StraightThroughEstimatorParam
    Former-commit-id: 4f84bbdef9bc42d269da769fee823cde92bd0f83
    Former-commit-id: 57d321015757dd6a8c8a6a437a9fef0e6ff1e2e8 [formerly 1ebb49bab48874ded74fe05c920564dc57c8be2f] [formerly 155f9ca50c66091abacd7f54b6fc8484973f2c93 [formerly eb3855bf323d1b08850bb1f11b7ed62e6f92bca8]] [formerly d871b1e6f48208bdae933ff5b7de0845a7c0bdba [formerly 152ab4286569c65f503ea8deb460f9225e576d7c] [formerly 130cf4755da99dcd97c204b8ebb63c15bcbb870f]]
    Former-commit-id: cd203b29e2fe041aa1da8af519e54d171ebc7fcf [formerly fd2bddfb0f195f5793090801425961cc2f54c33c] [formerly c6f471fc91ed32789e9df0b839e97aa8e7c0406a]
    Former-commit-id: 8a16df763d0a35672e8a940e384ca38a8b5edc73 [formerly 01c724874d85eb4f82c6570ad513c48ba0be42ce]
    Former-commit-id: df55ff543006092a4930e5c2c7181c69bc4ebe23
    Former-commit-id: d27a02ae162e03e8ccefde63edfa98762e15877c [formerly 19bec4aeba8af4a89bb284b357b0d700eaf7e49d]
    Former-commit-id: 2f7b98b6062af5b35bcbccd9776ca28ca1f7bdf0

--- Implementation/py/neural_network_v2_code/utils/StraightThroughEstimators.py
+++ Implementation/py/neural_network_v2_code/utils/StraightThroughEstimators.py
@@ -1,4 +1,5 @@
 import torch
+from utils.utils import itof2depth
 
 
 class STECleanFunc(torch.autograd.Function):
@@ -52,7 +53,6 @@ class STEThresholdFunc(torch.autograd.Function):
             - output tensor
         """
 
-        from utils.utils import itof2depth
         depth = itof2depth(x, 20e06)
         return torch.where(depth == 0, 0, 1)
     
@@ -99,3 +99,40 @@ class StraightThroughEstimator(torch.nn.Module):
             return STEThresholdFunc.apply(x)
         else:
             raise ValueError(f"Unknown task {self.task}")
+
+
+class StraightThroughEstimatorParam(torch.nn.Module):
+    """
+    Straight through estimator implementation
+    Implementation withoud the redefinition of the backward pass
+    """
+
+    def __init__(self, task: str) -> None:
+        """
+        Constructor
+        param:
+            - task: task to perform (clean or threshold)
+        """
+
+        super().__init__()
+        self.task = task
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass
+        param:
+            - x: input tensor
+        return:
+            - output tensor
+        """
+
+        if self.task == "clean":
+            hard_x = torch.where(abs(x) < 0.05, 0, x)  # Clean the itof data
+            return x + (hard_x - x).detach()  # Straight through estimator
+        elif self.task == "threshold":
+            depth = itof2depth(x, 20e06)
+            hard_x = torch.where(depth == 0, 0, 1)
+            return x + (hard_x - x).detach()  # Straight through estimator
+        else:
+            raise ValueError(f"Unknown task {self.task}")
+        
